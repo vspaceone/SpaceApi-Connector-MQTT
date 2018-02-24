31a75152afae2405e12372964629691b2a8fcb13
Damian Jesionek <Damian.Jes@gmx.de>
2018-02-24 20:36:25 +0100

    Working request handling

--- main.js
+++ main.js
@@ -16,6 +16,43 @@ function sendSpaceapiUpdate(api) {
         .end()
 }
 
+function handleRequest(topic, message){
+    unirest.get(config.get('spaceapi.server') + "/spaceapi")
+            .end(function (response) {
+                
+                if (response.statusType == 2){
+                    //console.log(topic + "|2|" + JSON.stringify(message))
+                    var requested = message.data.request
+                    requested = requested.substring(config.get('spaceapi.topics.spaceapi').length + 1).split(".")
+
+
+                    //console.log("Request: " + message.data.request.substring(config.get('spaceapi.topics.spaceapi').length));
+                    //console.log(requested);
+
+                    var responseData = JSON.parse(response.body);
+                    for (var i = 0; i < requested.length; i++){
+                        responseData = responseData[requested[i]]
+                    }
+
+                    //console.log("Response: " + responseData)
+
+                    client.publish(config.get('spaceapi.topics.spaceapi'),JSON.stringify({
+                        status:"ok",
+                        data:{
+                            request: message.data.request,
+                            response: responseData
+                        }
+                    }))
+                } else {
+                    client.publish(config.get('spaceapi.topics.spaceapi'),JSON.stringify({
+                        status:"error",
+                        error:"GET on " + config.get('spaceapi.server') + "/spaceapi failed returned code " + response.status
+                    }))
+                }
+
+                
+            })
+}
 
 // ##############################
 // "Main"
@@ -31,13 +68,13 @@ client.on('connect', function(){
     client.subscribe(config.get('spaceapi.topics.state'))
     client.subscribe(config.get('spaceapi.topics.temperature'))
     client.subscribe(config.get('spaceapi.topics.humidity'))
-
-    client.subscribe(config.get('spaceapi.topics.spaceapi'))
+    client.subscribe(config.get('spaceapi.topics.request'))
 })
 
-client.on('message', function (topic, buf){
+client.on('message', function (ttopic, buf){
+    var topic = ttopic;
     message = JSON.parse(buf)
-    console.log(message)
+    //console.log(topic + "|1|" + JSON.stringify(message))
 
     if (message.status != 'ok'){
         console.log("Error message: " + message.error);
@@ -71,27 +108,10 @@ client.on('message', function (topic, buf){
                 }]
             }
         })
-    } else if (topic == config.get('spaceapi.topics.spaceapi') && message.data.type == 'request'){
-
-        unirest.get(config.get('spaceapi.server') + "/spaceapi")
-            .end(function (response) {
-
-                if (response.statusType == 2){
-                    client.publish(config.get('spaceapi.topics.spaceapi'),JSON.stringify({
-                        status:"ok",
-                        data:{
-                            type: "response",
-                            spaceapi:JSON.parse(response.body)
-                        }
-                    }))
-                } else {
-                    client.publish(config.get('spaceapi.topics.spaceapi'),JSON.stringify({
-                        status:"error",
-                        error:"GET on " + config.get('spaceapi.server') + "/spaceapi failed returned code " + response.status
-                    }))
-                }
-
-                
-            })
+    } else if (topic == config.get('spaceapi.topics.request') 
+                && message.data.request.startsWith(config.get('spaceapi.topics.spaceapi'))){
+        handleRequest(topic, message)
     }
+        
+        
 })
